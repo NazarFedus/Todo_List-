b4df85c3263cdeb8f9f8a21ad2950b90177f959c
NazarFedus <simpleprograming7@gmail.com>
2023-04-25 12:44:08 +0100

    add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import TodoWrapper from "./components/TodoWrapper/TodoWrapper";
+import React, { FC } from "react";
 
-export default function App() {
+const App: FC = () => {
   return (
-    <TodoWrapper />
+    <div className="container-col h-[100vh]">
+      {/* InputList */}
+      <div className="container-col">
+        <h3>TodoInput</h3>
+        <div className="container-col">
+          <div>
+            <div><img src='' alt="" /></div>
+          <input type="text" placeholder="New Todo..." />
+          </div>
+
+          <button>Add new task</button>
+        </div>
+      </div>
+
+      {/* TodoList */}
+      <div className="container-col">
+        <h3>TodoList</h3>
+        <div className="container-col">
+          <div>
+            <button>All</button>
+            <button>Done</button>
+            <button>Todo</button>
+          </div>
+          <div></div>
+          <div>
+            <button>Delete done tasks</button>
+            <button>Delete all tasks</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default App;
--- src/types/list.ts
+++
@@ -1,6 +0,0 @@
-export interface ITodo {
-     id: number,
-     title: string,
-     completed: boolean
-}
-export interface IList extends Array<ITodo> {}
